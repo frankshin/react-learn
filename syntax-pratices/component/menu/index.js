9b9ce092cbc9206c9f5b5fb9db101659536d815d
xudengwei <xudw@lianlianpay.com>
2021-09-13 17:31:42 +0800

    ui updtae

--- syntax-pratices/component/menu/index.js
+++ syntax-pratices/component/menu/index.js
@@ -3,11 +3,16 @@ import React, { Component } from 'react'
 import routers from '../../routers'
 import { Link } from 'react-router-dom'
 
+import Style from './index.less'
 export default class MenuComponent extends Component {
+  componentDidMount () {
+    Style.use()
+  }
+
   render () {
     return (
       <nav>
-        <ul>
+        <ul className="menu">
           {
             routers.map((item, index) => {
               const { title, path } = item
@@ -21,5 +26,9 @@ export default class MenuComponent extends Component {
         </ul>
       </nav>
     )
+  }
+
+  componentWillUnmount () {
+    Style.unuse()
   }
 }
