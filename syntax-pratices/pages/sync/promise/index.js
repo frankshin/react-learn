e71308a320d871a726afbc2bb13764ea0ce3882c
xudengwei <xudw@lianlianpay.com>
2021-09-15 14:06:38 +0800

    updtae

--- syntax-pratices/pages/sync/promise/index.js
+++ syntax-pratices/pages/sync/promise/index.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import {
-  // test1,
-  // test2,
-  test3,
-} from './promiseLibs'
+import React, { useEffect } from 'react'
+// import Promise from './promiseLibs'
 
-export default function asyncPage () {
-  test3()
+export default function AsyncPage () {
+  useEffect(() => {
+    testPromise()
+  }, [])
+
+  const testPromise = () => {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(function () {
+        resolve({
+          a: 1,
+          b: 2,
+        })
+      }, 2000)
+    })
+    promise.then(
+      (res) => {
+        console.log('res', res)
+      },
+      (error) => {
+        console.log('error', error)
+      },
+    )
+  }
 
   return (
     <div>测试异步执行逻辑</div>
